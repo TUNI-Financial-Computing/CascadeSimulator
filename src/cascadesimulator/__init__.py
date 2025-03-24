c8d15875658255ad743eafd4307faec0f5dfee35
kbaltakys <kbaltakys@gmail.com>
2025-03-24 13:25:12 +0000

    Fix pyCascadeGenerator import #1

--- src/cascadesimulator/__init__.py
+++ src/cascadesimulator/__init__.py
@@ -1,4 +1,5 @@
 from cascadesimulator.cascade_generator_cpp import hello_from_bin
+from cascadesimulator._py_cascade_generator import pyCascadeGenerator
 
 def main() -> None:
     # Example main function usage
